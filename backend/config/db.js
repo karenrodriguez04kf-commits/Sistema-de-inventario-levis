9566ac31a104e61fd49bc9323b1dc4f3bd1401ee
maurorojas89 <sevenrojas17@gmail.com>
2026-09-08 16:44:05 -0500

    actualizar base_url para railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // Asegura que lea el archivo .env
 
-const db = mysql.createPool({ // Usamos Pool para mejor manejo de múltiples conexiones
-  
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'levis_db',
-  port: 3306,
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10
 });
 
-console.log('Conectado exitosamente a levis_db... 🚀');
+console.log('Conectado exitosamente a la base de datos de Railway... 🚀');
 
 module.exports = db;
