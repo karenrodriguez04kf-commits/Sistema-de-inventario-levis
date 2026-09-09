1c98bb3979183c23e2b5e8e716c89110ae79ff2d
maurorojas89 <sevenrojas17@gmail.com>
2026-09-08 19:26:18 -0500

    corregir mayusculas en importaciones de css

--- frontend/src/perfil.jsx
+++ frontend/src/perfil.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import api from './api'; 
 import { jwtDecode } from "jwt-decode";
-import './Perfil.css';
+import './perfil.css';
 
 const Perfil = () => {
     const [nombre, setNombre] = useState('');
