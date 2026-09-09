6e4b7518c1d53acc15d7cb2079d2fcf73417b252
maurorojas89 <sevenrojas17@gmail.com>
2026-09-08 21:08:10 -0500

    despliegue total v0.2

--- backend/server.js
+++ backend/server.js
@@ -67,8 +67,8 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3002;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log("-----------------------------------------");
-    console.log(`✅ Servidor LEVI'S listo en puerto ${PORT}`);
+    console.log(`✅ Servidor LEVI'S listo en el puerto ${PORT}`);
     console.log("-----------------------------------------");
 });
