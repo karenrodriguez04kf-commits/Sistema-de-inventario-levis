ed60701c9b1e9d739d3f968427e33e9bf717050d
maurorojas89 <sevenrojas17@gmail.com>
2026-09-08 20:27:54 -0500

    corregir db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql2');
-require('dotenv').config(); // Asegura que lea el archivo .env
+require('dotenv').config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10
+    host: process.env.MYSQLHOST || process.env.DB_HOST,
+    user: process.env.MYSQLUSER || process.env.DB_USER,
+    password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+    database: process.env.MYSQLDATABASE || process.env.DB_NAME,
+    port: process.env.MYSQLPORT || process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10
 });
 
 console.log('Conectado exitosamente a la base de datos de Railway... 🚀');
